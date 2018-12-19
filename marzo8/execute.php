0d820149f1b04a38f04467e83142c80919817db0
Lookatdrei <45816457+Lookatdrei@users.noreply.github.com>
2018-12-19 11:51:01 +0100

    Update execute.php

--- marzo8/execute.php
+++ marzo8/execute.php
@@ -47,24 +47,24 @@
 		//i parametri sono cosa voglio mandare indietro al mio utente, rimando il testo che
 		//ho ricevuto e che si trova nella variabile $text
 		$parameters = array('chat_id' => $chatId, "text" => $text);
-		if ($text=="data")
+		if ($text=="data"||$text=="/data")
 		{
 			$text="La data odierna è: ".date("d.m.y");
 			$parameters = array('chat_id' => $chatId, "text" => $text);
 			
 		}
-		if($text=="anni")
+		if($text=="anni"||$text=="/anni")
 		{
 			$text="L'età è 2004";
 			$parameters = array('chat_id' => $chatId, "text" => $text);
 		}
-		if ($text=="orario")
+		if ($text=="orario||$text=="/orario")
 		{
 			$text="L'ora attuale è: ".date("h.i.sa");
 			$parameters = array('chat_id' => $chatId, "text" => $text);
 			
 		}
-		if ($text=="foto")
+		if ($text=="foto"||$text=="/foto")
 		{
 			sendFoto($chatId,"foto.png",false,"La Mia Foto",$api);
 		
