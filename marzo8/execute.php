de0e18ebe7e8547aef2584393b58052ee8ab3c5d
Lookatdrei <45816457+Lookatdrei@users.noreply.github.com>
2019-01-09 10:41:22 +0100

    Update execute.php

--- marzo8/execute.php
+++ marzo8/execute.php
@@ -74,6 +74,15 @@
 			sendFoto($chatId,"foto.png",false,"La Mia Foto",$api);
 		
 		}
+		if($text=="barz")
+		{
+			$barz[0]="Cosa fa Pisano alle 4 di notte?Cerca le 2004"
+			$barz[1]="Un taglialegna va in un negozio di abbigliamento e chiede alla commessa: “Vorrei un paio di jeans.” E la signorina domanda: “Che taglia?” “La legna!!!”"
+			$barz[2]="Un consiglio per i medici: non mangiate una mela al giorno!"
+			$barz[3]="Qual è il colmo per un idraulico? Non capire un tubo!!!!"
+			$i= srand(0,3);
+			$parameters = array('chat_id' => $chatId, "text" => $barz[$i]);
+		}
 		
 	
 		
