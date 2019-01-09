a7fa05d86d45bbbbfbf8460541e9547ab3f2792b
Lookatdrei <45816457+Lookatdrei@users.noreply.github.com>
2019-01-09 11:13:26 +0100

    Update execute.php

--- marzo8/execute.php
+++ marzo8/execute.php
@@ -47,7 +47,7 @@
 		//i parametri sono cosa voglio mandare indietro al mio utente, rimando il testo che
 		//ho ricevuto e che si trova nella variabile $text
 		$parameters = array('chat_id' => $chatId, "text" => $text);
-		if ($text=="data"||$text=="/data")
+		if($text=="data"||$text=="/data")
 		{
 			$text="La data odierna è: ".date("d.m.y");
 			$parameters = array('chat_id' => $chatId, "text" => $text);
@@ -58,23 +58,22 @@
 			$text="L'età è 2004";
 			$parameters = array('chat_id' => $chatId, "text" => $text);
 		}
-		if ($text=="orario||$text=="/orario")
+		if($text=="orario||$text=="/orario")
 		{
 			$text="L'ora attuale è: ".date("h.i.sa");
 			$parameters = array('chat_id' => $chatId, "text" => $text);
 			
 		}
-		if ($text=="foto"||$text=="/foto")
 		
-		{
+		if($text=="foto"||$text=="/foto")
+				{
 			$foto[0]="foto.png";
 			$foto[1]="foto1.png";
 			$foto[2]="foto1.png";
 			$num=rand(0,2);
-			sendFoto($chatId,$foto[$num
-			],false,"La Mia Foto",$api);
-		
-		}
+			sendFoto($chatId,$foto[$num],false,"La Mia Foto",$api);
+				}
+				
 		if($text=="barz")
 		{
 			$barz[0]="Cosa fa Pisano alle 4 di notte?Cerca le 2004";
